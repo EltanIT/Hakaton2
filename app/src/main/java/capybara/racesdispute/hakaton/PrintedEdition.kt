85fd7efc6d0f797725b1d14aef7b53074cec8b21
polichinel <bezborodovstanislav109@gmail.com>
2022-12-03 11:00:32 +0300

    Add active

---
+++ app/src/main/java/capybara/racesdispute/hakaton/PrintedEdition.kt
@@ -0,0 +1,11 @@
+package capybara.racesdispute.hakaton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrintedEdition : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_printed_edition)
+    }
+}
