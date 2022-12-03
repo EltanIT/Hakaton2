b22543fd996c1e08bf9b6da032c0910c4a1e1c30
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-03 15:30:51 +0500

    new activity and fix problem

--- app/src/main/java/capybara/racesdispute/hakaton/admin/Application_list_admin.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/Application_list_admin.kt
@@ -2,7 +2,7 @@ package capybara.racesdispute.hakaton.admin
 
 import android.widget.RelativeLayout
 
-class Application_list_admin: Application_admin() {
+class Application_list_admin {
     private lateinit var rl1: RelativeLayout
 
     constructor(rl1: RelativeLayout) : super() {
