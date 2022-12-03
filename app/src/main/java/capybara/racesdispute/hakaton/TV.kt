d127c4f9a3effc8e3e68f2a984cc4c9707b853c2
polichinel <bezborodovstanislav109@gmail.com>
2022-12-03 16:18:57 +0300

    Add activity

---
+++ app/src/main/java/capybara/racesdispute/hakaton/TV.kt
@@ -0,0 +1,11 @@
+package capybara.racesdispute.hakaton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TV : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv)
+    }
+}
