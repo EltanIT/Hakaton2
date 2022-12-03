77753a0cae892e7364f2c66e82fff53027b5de77
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-03 12:43:32 +0500

    new activity

---
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/See_request.kt
@@ -0,0 +1,12 @@
+package capybara.racesdispute.hakaton.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import capybara.racesdispute.hakaton.R
+
+class See_request : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_request2)
+    }
+}
