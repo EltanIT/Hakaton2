bf10c8d9a6d3fa2140c182e87b27dabb86f9959a
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-03 11:52:19 +0500

    new activity

--- app/src/main/java/capybara/racesdispute/hakaton/See_request_Activity.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/See_request_Activity.kt
@@ -2,12 +2,18 @@ package capybara.racesdispute.hakaton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class See_request_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_request)
 
+
+    }
+
+
+    fun see_request_button_clicked(view: View) {
 
     }
 }
--- app/src/main/java/capybara/racesdispute/hakaton/submit.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/submit.kt
@@ -1,6 +1,8 @@
 package capybara.racesdispute.hakaton
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class submit: AppCompatActivity() {
@@ -8,5 +10,10 @@ class submit: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.submit)
 
+    }
+
+    fun BackToRegistrate(view: View) {
+        val intent = Intent(this@submit,registration::class.java)
+        startActivity(intent)
     }
 }
