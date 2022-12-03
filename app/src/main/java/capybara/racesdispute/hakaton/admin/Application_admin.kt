45fb94ee321eb68fc25abf4f71e5aebcc62c2c11
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-03 20:34:37 +0500

    connect activity

--- app/src/main/java/capybara/racesdispute/hakaton/admin/Application_admin.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/Application_admin.kt
@@ -1,20 +1,27 @@
 package capybara.racesdispute.hakaton.admin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.RelativeLayout
 import capybara.racesdispute.hakaton.R
+import capybara.racesdispute.hakaton.array.request_array
 
 open class Application_admin : AppCompatActivity() {
 
     private lateinit var rl1: RelativeLayout
-    private lateinit var array: ArrayList<Application_list_admin>
+    private lateinit var relativeArray: Array<RelativeLayout>
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application_admin)
 
-        array = ArrayList()
+        listView = findViewById(R.id.listView_1)
 
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, relativeArray)
+        listView.adapter = adapter
     }
 }
--- app/src/main/java/capybara/racesdispute/hakaton/admin/See_request.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/See_request.kt
@@ -3,13 +3,23 @@ package capybara.racesdispute.hakaton.admin
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import capybara.racesdispute.hakaton.R
 
 class See_request : AppCompatActivity() {
+
+    private lateinit var textviewanim: TextView
+    private lateinit var anim: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_see_request2)
+
+        textviewanim = findViewById(R.id.textviewanim)
+        anim = AnimationUtils.loadAnimation(this,R.anim.set)
     }
 
     fun go_to_setting_admin(view: View) {
@@ -23,6 +33,10 @@ class See_request : AppCompatActivity() {
     }
 
     fun see_request_button_clicked(view: View) {}
+
+    fun read_more(view: View) {
+        textviewanim.startAnimation(anim)
+    }
 }
 
 
