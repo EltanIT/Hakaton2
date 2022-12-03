f5cd4ad2c1e3df5ac7ffff1a613960f6a8d39ccc
polichinel <bezborodovstanislav109@gmail.com>
2022-12-03 13:56:03 +0300

    Add TryingConnectServer

--- app/src/main/java/capybara/racesdispute/hakaton/EnterActivity.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/EnterActivity.kt
@@ -2,10 +2,34 @@ package capybara.racesdispute.hakaton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.lifecycle.lifecycleScope
+import capybara.racesdispute.hakaton.server.AccountsRepository
+import kotlinx.coroutines.launch
 
+lateinit var email : AppCompatEditText
+lateinit var password : AppCompatEditText
 class EnterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter)
+        email = findViewById(R.id.EdTxtMail)
+        password = findViewById(R.id.EdTxtPassword)
+
+    }
+
+    fun Login(view: View) {
+        load()
+    }
+    fun load() {
+        lifecycleScope.launch {
+            val Username = email.text.toString()
+            val Password = password.text.toString()
+            val ac = AccountsRepository()
+            val token = ac.login(Username, Password)
+            Log.d("token", token.toString())
+        }
     }
 }
