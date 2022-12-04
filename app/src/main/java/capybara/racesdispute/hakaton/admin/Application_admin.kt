5beca7b20e179470367dc5bcea74c906eee49be6
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-04 12:50:36 +0500

    connect activity

--- app/src/main/java/capybara/racesdispute/hakaton/admin/Application_admin.kt
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/Application_admin.kt
@@ -1,13 +1,11 @@
 package capybara.racesdispute.hakaton.admin
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.RelativeLayout
 import capybara.racesdispute.hakaton.R
-import capybara.racesdispute.hakaton.array.request_array
 
 open class Application_admin : AppCompatActivity() {
 
---
+++ app/src/main/java/capybara/racesdispute/hakaton/admin/Setting_admin.kt
@@ -0,0 +1,18 @@
+package capybara.racesdispute.hakaton.admin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import capybara.racesdispute.hakaton.R
+
+class Setting_admin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_admin)
+    }
+}
